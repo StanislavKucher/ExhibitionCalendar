0be510879b09fb9d20269a594606c059167de0e9
Stanislav Kucher <StanislavKucher@ukr.net>
2020-04-13 17:13:10 +0300

    The following classes were added: User, EncodingFilter and simple validators

---
+++ src/main/java/com/exhibitionCalendar/model/entities/User.java
@@ -0,0 +1,13 @@
+package com.exhibitionCalendar.model.entities;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    //    private Enum role;
+    private String email;
+
+}
