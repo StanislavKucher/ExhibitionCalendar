011e8d74e70412dff932814f1e5e89127c11f0bb
Stanislav Kucher <kucherstanislav@gmail.com>
2020-05-21 22:08:20 +0300

    MySQLUserDAO complete.

--- src/main/java/com/exhibitionCalendar/model/dao/implementations/mySQL/MySQLUserDAO.java
+++ src/main/java/com/exhibitionCalendar/model/dao/implementations/mySQL/MySQLUserDAO.java
@@ -1,9 +1,16 @@
 package com.exhibitionCalendar.model.dao.implementations.mySQL;
 
+import com.exhibitionCalendar.model.connection.MySQLConnectionManager;
 import com.exhibitionCalendar.model.dao.interfaces.UserDAO;
+import com.exhibitionCalendar.model.entities.Role;
 import com.exhibitionCalendar.model.entities.User;
 import org.apache.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Locale;
 import java.util.Properties;
 
 public class MySQLUserDAO implements UserDAO<User, Integer> {
@@ -18,27 +25,126 @@ public class MySQLUserDAO implements UserDAO<User, Integer> {
 
     @Override
     public boolean create(User user) {
-        return false;
+        LOGGER.info("Create user starts with a user: " + user);
+        boolean result = false;
+        LOGGER.info("Query: " + properties.getProperty("insert"));
+        try (Connection connection = MySQLConnectionManager.getJndiConnection();
+             PreparedStatement statement = connection.prepareStatement(properties.getProperty("insert"))) {
+            setPreparedStatement(statement, user);
+            statement.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            LOGGER.error("SQLException was caught while adding a new user to DB" + e);
+            LOGGER.error(e);
+            e.printStackTrace();
+        }
+        LOGGER.info("User was add to DB successfully: " + result);
+        return result;
     }
 
     @Override
     public User retrieve(Integer userId) {
-        return null;
+        LOGGER.info("Method retrieve starts with a userId: " + userId);
+        User user = null;
+        LOGGER.info("Query: " + properties.getProperty("select"));
+        try (Connection connection = MySQLConnectionManager.getJndiConnection();
+             PreparedStatement statement = connection.prepareStatement(properties.getProperty("select"))) {
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet != null && resultSet.next()) {
+                user = getUserFromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            LOGGER.error("SQLException was caught while retrieving user info from DB" + e);
+            LOGGER.error(e);
+            e.printStackTrace();
+        }
+        LOGGER.info("Method retrieve returns the user: " + user);
+        return user;
     }
 
     @Override
     public boolean update(User user) {
-        return false;
+        LOGGER.info("Method update starts with a user: " + user);
+        boolean result = false;
+        LOGGER.info("Query: " + properties.getProperty("update"));
+        try (Connection connection = MySQLConnectionManager.getJndiConnection();
+             PreparedStatement statement = connection.prepareStatement(properties.getProperty("update"))) {
+            setPreparedStatement(statement, user);
+            statement.setInt(8, user.getUserID());
+            result = statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            LOGGER.error("SQLException was caught while updating user info in DB " + e);
+            LOGGER.error(e);
+            e.printStackTrace();
+        }
+        LOGGER.info("The user info was successfully updated: " + result);
+        return result;
     }
 
     @Override
     public boolean delete(Integer userId) {
-        return false;
+        LOGGER.info("Method delete starts with a userId" + userId);
+        boolean result = false;
+        LOGGER.info("Query: " + properties.getProperty("delete"));
+        try (Connection connection = MySQLConnectionManager.getJndiConnection();
+             PreparedStatement statement = connection.prepareStatement(properties.getProperty("delete"))) {
+            statement.setInt(1, userId);
+            result = statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            LOGGER.error("SQLException was caught while deleting a user from DB" + e);
+            e.printStackTrace();
+        }
+        LOGGER.info("A user was successfully deleted: " + result);
+        return result;
     }
 
     @Override
     public User retrieveByLogin(String login) {
-        return null;
+        LOGGER.info("Method retrieveByLogin starts with a login: " + login);
+        User user = null;
+        LOGGER.info("Query: " + properties.getProperty("selectByEmail"));
+        try (Connection connection = MySQLConnectionManager.getJndiConnection();
+             PreparedStatement statement = connection.prepareStatement(properties.getProperty("selectByLogin"))) {
+            statement.setString(1, login);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet != null && resultSet.next()) {
+                user = getUserFromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            LOGGER.error("SQLException was caught while getting user info by login from DB" + e);
+            LOGGER.error(e);
+            e.printStackTrace();
+        }
+        LOGGER.info("Method retrieveByLogin returns the user: " + user);
+        return user;
     }
 
+    // Supplementary method
+    private void setPreparedStatement(PreparedStatement statement, User user) throws SQLException {
+        LOGGER.info("SetPreparedStatement starts with parameters: statement: " + statement + ". And user: " + user);
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setString(3, user.getLogin());
+        statement.setString(4, user.getPassword());
+        statement.setString(5, user.getPhone());
+        statement.setString(6, user.getRole().name().toLowerCase(Locale.ENGLISH));
+        statement.setString(7, user.getEmail());
+        LOGGER.info("SetPreparedStatement ends with parameters: statement: " + statement + ". And user: " + user);
+    }
+
+    // Supplementary method
+    private User getUserFromResultSet(ResultSet resultSet) throws SQLException {
+        LOGGER.info("method madeUser start with resultSet: " + resultSet);
+        return new User.Builder()
+                .setUserID(resultSet.getInt("account_id"))
+                .setFirstName(resultSet.getString("first_name"))
+                .setLastName(resultSet.getString("last_name"))
+                .setLogin(resultSet.getString("login"))
+                .setPassword(resultSet.getString("password"))
+                .setPhone(resultSet.getString("phone"))
+                .setRole(Role.valueOf(resultSet.getString("role").toUpperCase(Locale.ENGLISH)))
+                .setEmail(resultSet.getString("email"))
+                .build();
+    }
 }
