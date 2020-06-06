d2f545fcf2eae2c6f3a1f995284b6061241e6090
Stanislav Kucher <kucherstanislav@gmail.com>
2020-06-06 16:23:36 +0300

    EncodingFilter was implemented.

--- src/main/java/com/exhibitionCalendar/controller/filters/EncodingFilter.java
+++ src/main/java/com/exhibitionCalendar/controller/filters/EncodingFilter.java
@@ -4,18 +4,27 @@ import javax.servlet.*;
 import java.io.IOException;
 
 public class EncodingFilter implements Filter {
+
+    private String code;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        code = filterConfig.getInitParameter("encoding");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String requestEncoding = servletRequest.getCharacterEncoding();
+        if (code != null && code.equalsIgnoreCase(requestEncoding)) {
+            servletRequest.setCharacterEncoding(code);
+            servletResponse.setCharacterEncoding(code);
+        }
 
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
     public void destroy() {
-
+        code = null;
     }
 }
